71d82628f6b803215d692c90d908c02984f8ae6d
Zlata Romanova <forofjob@gmail.com>
2023-02-21 10:21:21 +0700

    guessing game ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,42 @@
+const { diff } = require("semver");
+
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        if (this.min < this.max)
+            return Math.ceil((this.max + this.min) / 2)
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
-
 module.exports = GuessingGame;
+// let number = 15;
+// let game = new GuessingGame();
+// console.log(game);
+// game.setRange(0, 20)
+
+// console.log(game);
+// let result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+
+
+// console.log(result, number);
